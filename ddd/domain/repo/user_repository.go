1c67a33148a67a3598c7fb93d270a396442ca996
Administrator <qq2045891621@gmail.com>
2025-11-23 19:56:21 +0800

    feat(用户仓储): 添加更新用户信息的接口方法
    在用户仓储接口中添加UpdateUser方法，用于支持用户信息的更新操作

--- ddd/domain/repo/user_repository.go
+++ ddd/domain/repo/user_repository.go
@@ -7,8 +7,9 @@ import (
 
 // UserRepository 用户仓储接口
 type UserRepository interface {
-	CreateUser(ctx context.Context, userPo *po.UserPo) error
-	GetUserByAccount(ctx context.Context, account string) (*po.UserPo, error)
-	GetUserByUUID(ctx context.Context, userUUID string) (*po.UserPo, error)
-	ExistsByAccount(ctx context.Context, account string) (bool, error)
+    CreateUser(ctx context.Context, userPo *po.UserPo) error
+    GetUserByAccount(ctx context.Context, account string) (*po.UserPo, error)
+    GetUserByUUID(ctx context.Context, userUUID string) (*po.UserPo, error)
+    UpdateUser(ctx context.Context, userPo *po.UserPo) error
+    ExistsByAccount(ctx context.Context, account string) (bool, error)
 }
